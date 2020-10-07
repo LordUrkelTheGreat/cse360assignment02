6b156265500c3ee44a7d0eab707314dfcf9f2016
Sahil Patel <smpate11@asu.edu>
2020-10-07 13:55:04 -0700

    Initial AddingMachine.java file

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assignment02;
+
+/**
+ * This class is a simple adding machine that will add, subtract, and track the history of the total value
+ */
+
+public class AddingMachine {
+	private int total;
+	
+	/**
+	 * A constructor that will instantiate any Adding Machine object. Sets total to 0.
+	 */
+	public AddingMachine() {
+		total = 0;	// not needed - included for clarity
+	}
+	
+	/**
+	 * A method that returns the total value
+	 * @return total value
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/**
+	 * Add method that adds the parameter variable to the total value
+	 * @param value added to total
+	 */
+	public void add(int value) {
+		
+	}
+	
+	/**
+	 * Subtract method that subtracts the parameter variable from the total value
+	 * @param value subtracted from total
+	 */
+	public void subtract(int value) {
+		
+	}
+	
+	/**
+	 * @return string
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * Empties everything
+	 */
+	public void clear() {
+		
+	}
+	
+}
